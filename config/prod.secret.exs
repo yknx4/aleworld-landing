526de2790ae9c14ffdca7f3ad967314a476bd635
Ale Ornelas <afigueroa@goamv.com>
2021-04-21 19:57:18 -0500

    fix: secret use socket for db

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,16 +4,17 @@
 # remember to add this file to your .gitignore.
 use Mix.Config
 
-database_url =
-  System.get_env("DATABASE_URL") ||
-    raise """
-    environment variable DATABASE_URL is missing.
-    For example: ecto://USER:PASS@HOST/DATABASE
-    """
+# database_url =
+#   System.get_env("DATABASE_URL") ||
+#     raise """
+#     environment variable DATABASE_URL is missing.
+#     For example: ecto://USER:PASS@HOST/DATABASE
+#     """
 
 config :aleworld, Aleworld.Repo,
   # ssl: true,
-  url: database_url,
+  database: "myapp_dev",
+  socket_dir: "/var/run/postgresql",
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
 secret_key_base =
